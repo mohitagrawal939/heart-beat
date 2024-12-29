1a73c102a1391549bd634488e8eb5b2ee9768f1e
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-29 23:39:34 +0530

    connection updated

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 require("dotenv").config();
-require("./config/database");
+const { connectDB } = require("./config/database");
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("server is successfully listening on port 3000...");
-});
+connectDB()
+    .then(() => {
+        console.log("Database connection established...");
+        app.listen(3000, () => {
+            console.log("server is successfully listening on port 3000...");
+        });
+    })
+    .catch((err) => {
+        console.log("Database connot be connected...", err);
+    });
--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,6 @@ const connectDB = async () => {
     await mongoose.connect(process.env.DB_CONNECTION);
 };
 
-connectDB()
-    .then(() => {
-        console.log("Database connection established...");
-    })
-    .catch((err) => {
-        console.log("Database connot be connected...", err);
-    });
+module.exports = {
+    connectDB,
+};
