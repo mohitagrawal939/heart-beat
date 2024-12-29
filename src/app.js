f2c18017c8e69ca229c8ef72220087a9bdd7bbf5
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-30 03:37:23 +0530

    user and feed API created

--- src/app.js
+++ src/app.js
@@ -5,18 +5,51 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Mohit",
-        lastName: "Agrawal",
-        email: "mohitagrawal939@hmail.com",
-        password: "123456",
-    });
+    const user = new User(req.body);
     try {
         await user.save();
         res.send("User added successfully.");
     } catch (err) {
         res.status(400).send("Unable to add users.");
+    }
+});
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body?.email;
+    try {
+        const result = await User.findOne({ email: userEmail });
+        if (!result) {
+            res.status(404).send("No users found.");
+        } else {
+            res.send(result);
+        }
+    } catch (err) {
+        res.status(400).send("Unable to fetch user.");
+    }
+});
+
+app.delete("/user", async (req, res) => {
+    const id = req.body?.id;
+    try {
+        await User.findByIdAndDelete(id);
+        res.send("User deleted successfully.");
+    } catch (err) {
+        res.status(400).send("Unable to delete user.");
+    }
+});
+
+app.get("/feed", async (req, res) => {
+    try {
+        const result = await User.find({});
+        if (result?.length === 0) {
+            res.status(404).send("No users found.");
+        } else {
+            res.send(result);
+        }
+    } catch (err) {
+        res.status(400).send("Unable to fetch users.");
     }
 });
 
