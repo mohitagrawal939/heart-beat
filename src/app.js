3e6bfcd3eafdc5e61259ee46bbbff370b65c049f
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-30 00:24:48 +0530

    signup API testing started

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 require("dotenv").config();
 const { connectDB } = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: "Mohit",
+        lastName: "Agrawal",
+        email: "mohitagrawal939@hmail.com",
+        password: "123456",
+    });
+    try {
+        await user.save();
+        res.send("User added successfully.");
+    } catch (err) {
+        res.status(400).send("Unable to add users.");
+    }
+});
 
 connectDB()
     .then(() => {
