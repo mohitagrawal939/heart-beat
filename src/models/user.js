73cb61da1d90642c799ef8b9c449ffba40edadb7
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-30 13:59:34 +0530

    required check added

--- src/models/user.js
+++ src/models/user.js
@@ -4,14 +4,19 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
     },
     lastName: {
         type: String,
     },
     email: {
         type: String,
+        required: true,
     },
-    password: String,
+    password: {
+        type: String,
+        required: true,
+    },
     age: Number,
     gender: String,
 });
