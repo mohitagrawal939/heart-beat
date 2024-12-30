ccd890e98fa050fafad6293a3dfd400b8c7ab463
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-30 20:42:48 +0530

    JWT verification added

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ const User = require("./models/user");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
     try {
@@ -41,10 +43,35 @@ app.post("/login", async (req, res) => {
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if (isPasswordValid) {
+            const token = await jwt.sign(
+                { _id: user._id },
+                process.env.SECRET_KEY
+            );
+            res.cookie("token", token);
             res.send("Login Successful.");
         } else {
             throw new Error("Invalid credentials.");
         }
+    } catch (err) {
+        res.status(400).send("Error : " + err.message);
+    }
+});
+
+app.get("/profile", async (req, res) => {
+    try {
+        const cookies = req.cookies;
+        const { token } = cookies;
+        if (!token) {
+            throw new Error("Invalid token.");
+        }
+        const decodedMessage = await jwt.verify(token, process.env.SECRET_KEY);
+        const { _id } = decodedMessage;
+
+        const user = await User.findById(_id);
+        if (!user) {
+            throw new Error("User does not exist.");
+        }
+        res.send(user);
     } catch (err) {
         res.status(400).send("Error : " + err.message);
     }
