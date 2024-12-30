b3d31b7950c4ef0a257d6043fab99bbd25b8f2ff
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-31 02:15:52 +0530

    connect request route created

---
+++ src/routes/request.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/ConnectionRequest");
+
+const requestRouter = express.Router();
+
+requestRouter.post(
+    "/request/send/:status/:toUserId",
+    userAuth,
+    async (req, res) => {
+        try {
+            const fromUserId = req.user._id;
+            const toUserId = req.params.toUserId;
+            const status = req.params.status;
+
+            const connectRequest = new ConnectionRequest({
+                fromUserId,
+                toUserId,
+                status,
+            });
+            const data = await connectRequest.save();
+            res.json({
+                message: "Request sent",
+                data,
+            });
+        } catch (err) {
+            res.status(400).send("Error : " + err.message);
+        }
+    }
+);
+
+module.exports = requestRouter;
