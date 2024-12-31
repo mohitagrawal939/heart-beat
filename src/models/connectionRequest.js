42318385d27e75f1ade69f39fc96347937f561b8
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-31 19:30:04 +0530

    connection request schema fixed

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -4,10 +4,12 @@ const connectionRequestSchema = new mongoose.Schema(
     {
         fromUserId: {
             type: mongoose.Schema.Types.objectId,
+            ref: "User",
             required: [true, "fromUserId is required"],
         },
         toUserId: {
             type: mongoose.Schema.Types.objectId,
+            ref: "User",
             required: [true, "toUserId is required"],
         },
         status: {
@@ -15,7 +17,7 @@ const connectionRequestSchema = new mongoose.Schema(
             required: [true, "status is required"],
             enum: {
                 values: ["ignore", "interested", "accepeted", "rejected"],
-                message: "(VALUE} is incorrect status type",
+                message: "(VALUE} is incorrect status type.",
             },
         },
     },
@@ -24,10 +26,14 @@ const connectionRequestSchema = new mongoose.Schema(
     }
 );
 
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
 connectionRequestSchema.pre("save", function (next) {
     const connectRequest = this;
 
-    if (connectRequest.toUserId === connectRequest.fromUserId) {
+    // as we are comparing objectIds, we need to convert them to string to compare.
+    // if we dont want to convert them to string, we can use .equals method of mongoose
+    if (connectRequest.toUserId.equals(connectRequest.fromUserId)) {
         throw new Error("You can't send request to yourself");
     }
     next();
