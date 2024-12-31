3706f1c541cde117ab9ac9d1fec56a81051654c7
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-31 19:20:15 +0530

    msg corrected

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,13 +5,13 @@ const userAuth = async (req, res, next) => {
     try {
         const { token } = req.cookies;
         if (!token) {
-            throw new Error("Token is not valid!!!!!!!!!");
+            throw new Error("Token is not valid!");
         }
         const decodedObj = await jwt.verify(token, process.env.SECRET_KEY);
         const _id = decodedObj;
         const user = await User.findById(_id);
         if (!user) {
-            throw new Error("User not found");
+            throw new Error("User not found.");
         }
         req.user = user;
         next();
