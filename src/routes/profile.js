07860de90f9d48b2721b2a1777f63f7761b235bd
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-31 19:43:50 +0530

    Profile edit route added

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validations");
 
 const profileRouter = express.Router();
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
     try {
         const user = req.user;
         res.send(user);
     } catch (err) {
         res.status(400).send("Error : " + err.message);
+    }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+    try {
+        if (!validateEditProfileData(req.body)) {
+            throw new Error("Invalid edit request");
+        }
+
+        const loggedInUser = req.user;
+
+        Object.keys(req.body).forEach((key) => {
+            loggedInUser[key] = req.body[key];
+        });
+
+        res.json({
+            message: `${loggedInUser.firstName} Profile updated successfully`,
+            user: loggedInUser,
+        });
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
     }
 });
 
