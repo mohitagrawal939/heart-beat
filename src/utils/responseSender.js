0717a42263ad7326b6d60293d9749ad76a7237c6
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-31 19:27:47 +0530

    response sender central function created

---
+++ src/utils/responseSender.js
@@ -0,0 +1,16 @@
+const sendResponse = (res, statusCode, data = null, message = "") => {
+    const response = {
+        success: statusCode >= 200 && statusCode < 300,
+        statusCode,
+        message:
+            message ||
+            (statusCode >= 200 && statusCode < 300
+                ? "Request was successful"
+                : "An error occurred"),
+        data,
+    };
+
+    res.status(statusCode).json(response);
+};
+
+module.exports = sendResponse;
